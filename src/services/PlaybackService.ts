6bbeb667ff5b4e4b8fec1eae7ba3d7fd2150d3a1
Rafael César <rafael.cesar@playlist.com.br>
2025-07-29 16:51:33 -0300

    fix: corrige sobreposição de áudio
    Corrige o bug que permitia que o app tocasse uma faixa enquanto outro som já estava ativo. Agora o player pausa outros aplicativos antes de começar.

--- src/services/PlaybackService.ts
+++ src/services/PlaybackService.ts
@@ -3,4 +3,11 @@ import TrackPlayer, {Event} from 'react-native-track-player';
 export const PlaybackService = async function () {
   TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
   TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+  TrackPlayer.addEventListener(Event.RemoteDuck, async ({permanent}) => {
+    if (permanent) {
+      await TrackPlayer.stop();
+      return;
+    }
+    await TrackPlayer.pause();
+  });
 };
