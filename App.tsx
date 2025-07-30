061cda14fbaf9ba9b20e75d72f8e73cd9a68c5fe
Rafael César <rafael.cesar@playlist.com.br>
2025-07-30 11:50:19 -0300

    feat: adiciona suporte a deep linking
    - Adicionada a configuração `linking` ao `NavigationContainer` para habilitar a navegação no aplicativo via links de compartilhamento (deep linking).
    
    - O `ContextProvider` foi implementado na raiz do aplicativo para prover o estado global (variáveis e funções) a todos os componentes da árvore.
    
    - Realizada a migração do `SafeAreaView` da biblioteca `react-native` para a `react-native-safe-area-context`. Esta mudança garante um tratamento mais robusto das áreas seguras do dispositivo (como notches e a barra de status), aplicando `padding` de forma automática para prevenir a sobreposição de componentes.
    
    - Adicionado o componente global `NoConnection`, que exibe um aviso em toda a aplicação quando o dispositivo perde a conexão com a internet.

--- App.tsx
+++ App.tsx
@@ -7,17 +7,17 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-
+import {StatusBar} from 'react-native';
 import Navigation from './src/navigation';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Toast, {ErrorToast, BaseToast} from 'react-native-toast-message';
-
+import {ContextProvider} from '@/hooks/context';
 import 'react-native-reanimated';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {NoConnection} from '@/components/NoConnection';
 
 function App(): React.JSX.Element {
   const queryClient = new QueryClient();
-
   const toastConfig = {
     error: (props: any) => (
       <ErrorToast
@@ -35,16 +35,41 @@ function App(): React.JSX.Element {
     ),
   };
 
+  const liking = {
+    prefixes: [
+      'https://planner.stationoffice.com.br',
+      'http://planner.stationoffice.com.br',
+    ],
+    config: {
+      screens: {
+        MainScreen: {
+          path: 'app',
+          screens: {
+            Podcast: {
+              path: 'podcast',
+              screens: {
+                SharedEpisode: 'episode/:id',
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <StatusBar barStyle={'light-content'} backgroundColor={'#000'} />
-      <NavigationContainer>
-        <QueryClientProvider client={queryClient}>
-          <Navigation />
-        </QueryClientProvider>
-      </NavigationContainer>
-      <Toast config={toastConfig} />
-    </SafeAreaView>
+    <ContextProvider>
+      <SafeAreaView style={{flex: 1}}>
+        <NoConnection />
+        <StatusBar barStyle={'light-content'} backgroundColor={'#000'} />
+        <NavigationContainer linking={liking}>
+          <QueryClientProvider client={queryClient}>
+            <Navigation />
+          </QueryClientProvider>
+        </NavigationContainer>
+        <Toast config={toastConfig} />
+      </SafeAreaView>
+    </ContextProvider>
   );
 }
 
